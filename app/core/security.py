2f7112ad90694c5dee9720105e0a1dae6b949c25
limouren <XiaoBai_12138@yeah.net>
2023-09-03 13:03:24 +0800

    Merge remote-tracking branch 'origin/main'

--- app/core/security.py
+++ app/core/security.py
@@ -1,19 +1,4 @@
 # 在 core/security.py 文件中
-from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from sqlalchemy.orm import Session
-from app.models.user import User
-from app.core.database import _DATABASE_
+from fastapi.security import HTTPBasic
 
 security = HTTPBasic()
-
-
-def get_user(db: Session, username):
-    return db.query(User).filter(User.username == username).first()
-
-
-def authenticate(credentials: HTTPBasicCredentials = Depends(security), db: Session = Depends(_DATABASE_)):
-    user = get_user(db, credentials.username)
-    if not user or not user.check_password(credentials.password):
-        return {"code": 401, "detail": "Incorrect username or password"}
-    return credentials.username
